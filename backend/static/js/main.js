aa0c13c0adecb1d758ec122873076d7dc32b3a8d
RedHot099 <redhotdevil996@gmail.com>
2023-09-07 08:11:49 +0200

    move JS functions into static files

---
+++ backend/static/js/main.js
@@ -0,0 +1,78 @@
+//function to get session token
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+//functions performed after document load
+$(document).ready(function() {
+    //Write icon onclick action
+    $('i#write').on('click', function(event) {
+        var cardId = $(this).closest('.card').data('card-id');
+        window.location.href = '/'+cardId;
+    });
+    // Handle delete icon click
+    $('i#delete').on('click', function(event) {
+        event.preventDefault();
+        
+        var cardId = $(this).closest('.card').data('card-id');
+        var deleteUrl = '/api/' + cardId + '/';
+
+        var csrftoken = getCookie('csrftoken');
+
+        $.ajax({
+            type: 'DELETE',
+            url: deleteUrl,
+            headers: {
+                'X-CSRFToken': csrftoken
+            },
+            success: function(response) {
+                alert(response.res);
+                window.location.href = '/';
+            },
+            error: function(error) {
+                alert('Error deleting: ' + JSON.stringify(error));
+            }
+        });
+    });
+
+    //Handle create icon click 
+    $('i#create').on('click', function(event) {
+        event.preventDefault();
+        
+        var cardId = $(this).closest('.card').data('card-id');
+        var url = '/api/create/' + cardId + '/';
+
+        var csrftoken = getCookie('csrftoken');
+
+        $.ajax({
+            type: 'POST',
+            url: url,
+            headers: {
+                'X-CSRFToken': csrftoken
+            },
+            success: function(response) {
+                alert(response.res);
+                window.location.href = '/api/'+cardId;
+            },
+            error: function(error) {
+                alert('Error deleting: ' + JSON.stringify(error));
+            }
+        });
+    });
+
+    //zaplecze card onclick toggle
+    $('.card-body').on('click', function() {
+        var table = $(this).parent().find('.additional-attributes');
+        table.toggle();
+    });
+});
---
+++ backend/static/js/zaplecze.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+    const catSlider = document.getElementById('CatSlider');
+    const catSliderValue = document.getElementById('CatSliderValue');
+
+    catSlider.addEventListener('input', function () {
+        catSliderValue.value = catSlider.value;
+    });
+    const subCatSlider = document.getElementById('SubCatSlider');
+    const subCatSliderValue = document.getElementById('SubCatSliderValue');
+
+    subCatSlider.addEventListener('input', function () {
+        subCatSliderValue.value = subCatSlider.value;
+    });
+
+
+    $('#catForm').on('submit', function(event) {
+        event.preventDefault(); // Prevent form from submitting normally
+        
+        // Serialize form data
+        var formData = $(this).serialize();
+        console.log(formData);
+
+        // Check if any field is empty
+        var emptyFields = false;
+        $(this).find('input').each(function() {
+            if ($(this).val() === '') {
+                emptyFields = true;
+                return false; // Exit the loop early
+            }
+        });
+        
+        if (emptyFields) {
+            alert('Please fill in all fields.');
+            return; // Do not proceed with submission
+        }
+
+        // Send POST request to the /api/all/ endpoint
+        $.ajax({
+            type: 'POST',
+            url: '/api/structure/{{data.id}}/',
+            data: formData,
+            success: function(response) {
+                // Handle success response here
+                window.location.href = '/';
+            },
+            error: function(error) {
+                // Handle error response here
+                console.log(error);
+            }
+        });
+    });
+});
