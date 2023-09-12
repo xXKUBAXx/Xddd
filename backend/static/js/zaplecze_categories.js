d1cef23b9c394b3c6db8572604144fb312a05694
RedHot099 <redhotdevil996@gmail.com>
2023-09-12 13:35:01 +0200

    write form->API connection ~TODO:fix time format

--- backend/static/js/zaplecze_categories.js
+++ backend/static/js/zaplecze_categories.js
@@ -20,6 +20,14 @@ $(document).ready(function() {
         d_value.val(d_slider.val());
     });
 
+    const today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1;
+    let yyyy = today.getFullYear();
+    dd < 10 ? dd = '0' + dd : null;
+    mm < 10 ? mm = '0' + mm : null; 
+    document.getElementById("start_date").setAttribute("value", yyyy + '-' + mm + '-' + dd);
+
     $("button#categories").on('click', (event) => {
         const cardId = $('#main').data('card-id');
         $.ajax({
@@ -49,14 +57,16 @@ $(document).ready(function() {
                 });
                 $("button#categories").remove();
                 $("#articles").show();
-                $.ajax({
-                    type: 'PUT',
-                    url: '/api/' + cardId + '/',
-                    headers: {
-                        'X-CSRFToken': getCookie('csrftoken')
-                    },
-                    data: {"wp_post_count": response.length}
-                });
+                if (response.length != $("#wp_post_count").innerHTML){
+                    $.ajax({
+                        type: 'PUT',
+                        url: '/api/' + cardId + '/',
+                        headers: {
+                            'X-CSRFToken': getCookie('csrftoken')
+                        },
+                        data: {"wp_post_count": response.length}
+                    });
+                }
             },
             error: (e) => {
                 console.error(e)
@@ -65,6 +75,7 @@ $(document).ready(function() {
     });
     $("#articles").on('click', function(event) {
         const ul = document.createElement("ul");
+        ul.setAttribute("id", "selected_cats")
         const rows = document.getElementById("cat_table").getElementsByTagName("tr");
         for (let i = 0; i < rows.length; i++) {
             const checkbox = rows[i].getElementsByTagName("input")[0];
@@ -72,6 +83,9 @@ $(document).ready(function() {
                 const rowData = rows[i].textContent.trim();
                 li = document.createElement("li");
                 li.textContent = rowData;
+                li.setAttribute("data-id", checkbox.id);
+                li.setAttribute("data-name", checkbox.name);
+                li.append(document.createElement("ul"))
                 ul.append(li);
             }
         }
@@ -79,5 +93,48 @@ $(document).ready(function() {
         $("#cat_table").parent().remove();
         $(this).remove();
         $("#writeForm").show();
+    });
+
+    $("#writeForm").on('submit', function(event) {
+        event.preventDefault();
+        let formData = $(this).serialize();
+        formData = formData.slice(0, formData.indexOf("start_date")) + formData.slice(formData.indexOf("start_date")+formData.slice(formData.indexOf("start_date")).indexOf("&")+1);
+        let start_date = Date.parse($("#writeForm > div > [name='start_date']").val());
+        // start_date = new Date(start_date.getFullYear(), start_date.getMonth(), start_date.getDate());
+        days_delta = $("#writeForm > div > [name='days_delta']").val();
+        const cardId = $('#main').data('card-id');
+        const cats = document.getElementById("selected_cats").getElementsByTagName("li");
+
+        const spin = document.createElement("div");
+        spin.classList.add("spinner");
+        $(this).parent().append(spin);
+        $(this).remove();
+        for (i of cats) {
+            cat_data = formData;
+            cat_data += "&cat_id="+i.getAttribute("data-id");
+            cat_data += "&category="+i.getAttribute("data-name");
+            cat_data += "&start_date="+start_date;
+            start_date += days_delta;
+            console.log(cat_data);
+            last_id = i.getAttribute("data-id");
+            $.ajax({
+                type: 'POST',
+                url: '/api/write/'+ cardId +'/',
+                headers: {
+                    'X-CSRFToken': getCookie('csrftoken')
+                },
+                data: cat_data,
+                success: function(response) {
+                    response.urls.forEach(function(e){
+                        $("#selected_cats > [data-id=\'"+response.id+"\'] > ul")
+                        .append("<li><a href=\""+e+"\">"+e+"</a></li>");
+                        response.id == last_id ? $("div.spinner").remove() : null;
+                    })
+                },
+                error: function(response) {
+                    console.error(response);
+                }
+            })
+        }
     });
 });
--- backend/views/zaplecze_write.py
+++ backend/views/zaplecze_write.py
@@ -8,6 +8,7 @@ from django.views.generic import View
 
 
 from ..src.CreateWPblog.openai_article import OpenAI_article
+from datetime import datetime
 
 class ZapleczeWrite(APIView):
     def get_object(self, zaplecze_id):
@@ -25,3 +26,37 @@ class ZapleczeWrite(APIView):
             )
         serializer = ZapleczeSerializer(zaplecze)
         data = serializer.data
+        category, id, openai_key, a, p = \
+                str(request.data.get('category')), \
+                request.data.get('cat_id'), \
+                str(request.data.get('openai_api_key')), \
+                int(request.data.get('a_num')), \
+                int(request.data.get('p_num'))
+        
+
+        params = {
+            "api_key" : openai_key,
+            "domain_name" : data['domain'], 
+            "wp_login" : data['wp_user'], 
+            "wp_pass" : data['wp_api_key'],
+            "lang" : data['lang']
+        }
+
+        if 'start_date' in request.data:
+            params['start_date'] = datetime.strptime(request.data.get('start_date'), "%Y-%m-%d")
+        if 'days_delta' in request.data:
+            params['days_delta'] = int(request.data.get('days_delta'))
+        if 'forward_delta' in request.data:
+            params['forward_delta'] = bool(request.data.get('forward_delta'))
+        
+        o = OpenAI_article(**params)
+
+        titles, cat_id = o.create_titles(category, a, id)
+        print(category+" - created titles: \n - " + "\n - ".join(titles))
+
+        urls = []
+        for t in titles:
+            urls.append(o.create_article(p, t, cat_id, True, "backend/src/CreateWPblog/"))
+
+        return Response({"urls": urls, "id": id}, status=status.HTTP_201_CREATED)
+    
