71b773dc41950c1dd1efd1fba6455fa6d27db33c
RedHot099 <redhotdevil996@gmail.com>
2023-09-12 08:00:13 +0200

    write form on input functionality

--- backend/static/js/zaplecze_categories.js
+++ backend/static/js/zaplecze_categories.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
+    // Select sliders and value elements 
+    const art_slider = $("#ArtSlider");
+    const art_value = $("#ArtSliderValue");
+    const p_slider = $("#ParagraphsSlider");
+    const p_value = $("#ParagraphsSliderValue");
+    const d_slider = $("#DaysSlider");
+    const d_value = $("#DaysSliderValue");
+
+    // Add an input event listener 
+    art_slider.on('input', function() {
+        art_value.val(art_slider.val());
+    });
+
+    p_slider.on('input', function() {
+        p_value.val(p_slider.val());
+    });
+
+    d_slider.on('input', function() {
+        d_value.val(d_slider.val());
+    });
+
     $("button#categories").on('click', (event) => {
-        var cardId = $('#main').data('card-id');
+        const cardId = $('#main').data('card-id');
         $.ajax({
             type: 'GET',
             url: '/api/structure/'+ cardId +'/',
