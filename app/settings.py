dc14e2f7c3c7702ad22cb0a7d1bf561a490a9b31
RedHot099 <redhotdevil996@gmail.com>
2023-09-29 10:01:25 +0200

    static dir update

--- app/settings.py
+++ app/settings.py
@@ -141,18 +141,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+import os
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'backend/static')
+]
+# STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT='/home/admin/actions-runner/work/django/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os 
+ 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'backend/static')
-]
+
 
 AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
