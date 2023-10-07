03f57b82ab0582039d135d662f91bec2d65ab333
RedHot099 <redhotdevil996@gmail.com>
2023-10-07 12:06:33 +0200

    add openai api key property to user profile

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Zaplecze
+from .models import Zaplecze, Account
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 
@@ -8,3 +10,18 @@ class ZapleczeAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Zaplecze, ZapleczeAdmin)
+
+
+
+class AccountInline(admin.StackedInline):
+    model = Account
+    can_delete = False
+    verbose_name_plural = 'Accounts'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (AccountInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+admin.site.register(Account)
