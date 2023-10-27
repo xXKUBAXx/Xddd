2a7ecd63427d7950bb6508bb2dc20f7afaf3af37
RedHot099 <redhotdevil996@gmail.com>
2023-10-27 11:46:18 +0200

    add rest api endpoint for writing on multiple site

--- backend/urls.py
+++ backend/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path('api/structure/', views.AnyZapleczeAPIStructure.as_view()),
     path('api/write/<int:zaplecze_id>/', views.ZapleczeWrite.as_view()),
     path('api/write/', views.AnyZapleczeWrite.as_view()),
+    path('api/links/', views.ManyZapleczesWrite.as_view()),
     path('', views.Front.as_view()),
     path('logout', views.logout_view),
     path('user/', views.UpdateProfile.as_view()),
