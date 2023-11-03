d00002e18ac686b2a5d0e56d0fe8b501ea9a9ade
Dragone <arcxar@gmail.com>
2023-11-03 14:46:32 +0100

    Made some fixes of classic zapleczara
    Zapleczara is usable now

--- backend/static/js/zaplecze_classic.js
+++ backend/static/js/zaplecze_classic.js
@@ -33,6 +33,38 @@ $(document).ready(function() {
         });
     });
 
+    // function getWordpressPostsTitles() {
+
+    //     const password = document.getElementById('wp_api_key').innerText;
+    //     const domain = document.getElementById('domain').innerText;
+    //     const login = document.getElementById('wp_user').innerText;
+      
+    //     const token = btoa(`${login}:${password}`);
+    //     const authHeader = { 'Authorization': `Basic ${token}` };
+      
+    //     const apiUrl = `https://${domain}/wp-json/wp/v2/posts`;
+    //     console.log(apiUrl);
+      
+    //     fetch(apiUrl, { headers: authHeader })
+    //       .then(response => {
+    //         if (!response.ok) {
+    //           throw new Error(`HTTP error! status: ${response.status}`);
+    //         }
+    //         return response.json();
+    //       })
+    //       .then(posts => {
+    //         console.log('Tytuły wpisów:');
+    //         posts.forEach(post => {
+    //           console.log(post.title.rendered);
+    //         });
+    //       })
+    //       .catch(error => {
+    //         console.error('Wystąpił błąd podczas pobierania wpisów:', error);
+    //       });
+    //   }
+      
+    //   setInterval(getWordpressPostsTitles, 30000);
+
     function getCookie(name) {
         let cookieValue = null;
         if (document.cookie && document.cookie !== '') {
