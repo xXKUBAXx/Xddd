51b2fac7b2798b560620513153d0b30042767b8d
RedHot099 <redhotdevil996@gmail.com>
2023-11-28 10:02:40 +0100

    loggin fix

--- backend/views/utils.py
+++ backend/views/utils.py
@@ -4,7 +4,11 @@ def log_user():
     def wrapped(f):
         def wrapper(request, *args, **kwargs):
             # Log the message with user info and additional message
-            logging.getLogger(__name__).info(f'{request.user.email} -> {request.path}')
+            try:
+                email = request.user.mail
+            except:
+                email = "unlogged"
+            logging.getLogger(__name__).info(f'{email} -> {request.path}')
 
             # Call the original view function
             response = f(request, *args, **kwargs)
