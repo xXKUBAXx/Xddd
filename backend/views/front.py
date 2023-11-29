4d7c311c1e63e9738f5841012769ab95b52a85f5
RedHot099 <redhotdevil996@gmail.com>
2023-11-29 07:37:14 +0100

    serve non logged users

--- backend/views/front.py
+++ backend/views/front.py
@@ -203,9 +203,13 @@ class WriteLink(View):
 @method_decorator(log_user(), name='dispatch')
 class LinksPanel(View):
     def get(self, request):
-        user = request.user.email.split("@")[0]
-        with urllib.request.urlopen(f"https://panel.verseo.pl/get_client_eichner.php?token=45h5j56k6788i4y3h57k567i54t3w6ki5y6u4u6h&pozycjoner={user}&aktywne=1") as url:
-            umowy = json.load(url)
+        try:
+            user = request.user.email.split("@")[0]
+            with urllib.request.urlopen(f"https://panel.verseo.pl/get_client_eichner.php?token=45h5j56k6788i4y3h57k567i54t3w6ki5y6u4u6h&pozycjoner={user}&aktywne=1") as url:
+                umowy = json.load(url)
+        except AttributeError:
+            umowy = {"data": {"key":""}}
+
 
         try:
             papaj_spi = User.objects.get(username='papaj_spi')
@@ -221,7 +225,11 @@ class LinksPanel(View):
 class UpdateProfile(APIView):
     def get(self, request):
         #get all table rows that belongs to current user
-        queryset = Link.objects.filter(user=request.user.email)
+        try:
+            queryset = Link.objects.filter(user=request.user.email)
+        except AttributeError:
+            print("Anonymus user got into user page!")
+            queryset = []
         context = {
             "data": queryset[::-1]
         }
